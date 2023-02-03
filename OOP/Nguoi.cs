ff252a86106a64ac6fed8b5a84cc32e0c2a2bfa3
shanghaik168@gmail.com <Shanghaik1998>
2023-02-03 13:16:20 +0700

    oop

---
+++ OOP/Nguoi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    internal class Nguoi
+    {
+        // Các thành phần chính trong 1 class
+        // 1. Các thuộc tính
+        string ten = "Lê văn A"; // giá trị mặc định ngoài constructor
+        int tuoi = 10;
+        string cccd = "Không có";
+        double chieucao = 200;
+        double cannang = 70;
+        // 2. Constructor - Hàm tạo
+        // - Tạo các thuộc tính cho đối tượng, Tạo đối tượng
+        // Có 2 loại hàm tạo (không tham số - có tham số)
+        public Nguoi() // ctor tab tab
+        {
+            // constructor không tham số
+            // Tạo ra các đối tượng với các thuộc tính mang giá
+            // trị mặc định
+            ten = "Lê Văn B"; tuoi = 20; cccd = "1234"; // mặc định
+        }
+        public Nguoi(string ten, int tuoi, string cccd, double chieucao, double cannang)
+        { // Tạo ra các đối tượng với các thuộc tính có giá trị được truyền vào
+            this.Ten = ten;
+            this.Tuoi = tuoi;
+            this.Cccd = cccd;
+            this.Chieucao = chieucao;
+            this.Cannang = cannang;
+        }
+        // 3. Property: Cho phép ta truy cập đến các thuộc tính của đối tượng một cách gián tiếp
+        // Bôi đen thuộc tính muốn tạo -> chuột phải -> Quick action -> Encapsulate .. property
+        // get: lấy giá trị, set: gán giá trị
+        public string Ten { get => ten; set => ten = value; }
+        public int Tuoi { get => tuoi; set => tuoi = value; }
+        public string Cccd { get => cccd; set => cccd = value; }
+        public double Chieucao { get => chieucao; set => chieucao = value; }
+        public double Cannang { get => cannang; set => cannang = value; }
+        // 4. Các phương thức của class - Các hành vi của đối tượng
+        public void InThongTin()
+        {
+            Console.WriteLine($"Tên: {ten}, tuổi: {tuoi}, CCCD: {cccd}");
+        }
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace OOP
+{
+    internal class Program
+    {
+        /*
+         * OOP - Object Oriented Programing - Lập trình hướng đối
+         * tượng là phương pháp lập trình cho phép ta tạo ra và
+         * thao tác trên các đối tượng cụ thể vd như: con người,
+         * động vật, nhà cửa,... 
+         * Đối tượng - object: Bất kì sự vật, hiện tượng nào sở 
+         * hữu các thuộc tính (đặc điểm), có thể hành động và có
+         * thể được quản lý thì đều là đối tượng
+         * Chúng ta không trực tiếp tạo ra các đối tượng đơn lẻ
+         * mà sử dụng 1 thành phần là Class (lớp) để tạo ra đối
+         * tượng. Mỗi class là một nguyên mẫu của đối tượng, mỗi
+         * đối tượng được gọi là 1 thể hiện của class (instance)
+         * Trong OOP có 4 tính chất cơ bản mà chúng ta cần nắm được
+         * + Tính bao đóng (đóng gói): Thể hiện khả năng truy cập
+         * + Tính kế thừa: Thể hiện khả năng kế thừa
+         * + Tính trừu tượng: Trừu tượng hóa một loại đối tượng
+         * + Tính đa hình: Thể hiện các mặt khác nhau trong cùng 1
+         * hành vi của các đối tượng
+         */
+        // Tạo class Chuột phải vào project -> New items
+        // -> Class -> Đặt tên -> Add
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Nguoi nguoi = new Nguoi(); // Khởi tạo 1 đối tượng với ctr không tham số
+            nguoi.InThongTin(); // Gọi phương thức từ lớp Nguoi
+            Nguoi nguoi2 = new Nguoi("Khánh", 19, "123", 120, 100); // có tham số
+            nguoi2.InThongTin();
+     
+        }
+    }
+}
